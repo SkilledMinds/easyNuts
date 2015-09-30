836fe0a7bf6c998e06a27246d598e309af43c235
SkilledMinds <gyan.cloudpobox@gmail.com>
2015-09-30 13:47:39 +0530

    Update ProducerConsumerQ.java

--- code-28-10-2014/ProducerConsumerQ.java
+++ code-28-10-2014/ProducerConsumerQ.java
@@ -1,124 +1,121 @@
 // Queue
-class CustomerQueue {
+class CustomerQueue
+{
 
-	String sharedCustomerQueue[] = new String[5];
+    String sharedCustomerQueue[] = new String[5];
 }
 
 // Client
-public class ProducerConsumerQ {
+public class ProducerConsumerQ
+{
 
-	static int NEED_TO_SERVE_BATCH = 2;
+    static int NEED_TO_SERVE_BATCH = 2;
 
-	public static void main(String[] args) {
-		CustomerQueue cq = new CustomerQueue();
-		Thread pro = new producer(cq);
-		Thread con = new consumer(cq);
-		pro.setName("Producer");
-		con.setName("Consumer");
-		pro.start();
-		con.start();
+    public static void main(String[] args)
+    {
 
-	}
+        System.out.println(" Producer will fill SharedQueue with 5 elements in each batch.");
+        System.out.println(" Consumer will consume elements available in SharedQueue in each batch.");
 
-}
+        System.out.println(" Total " + NEED_TO_SERVE_BATCH + " will be served.");
+        System.out.println(" \n ***************************************************************************** ");
+        CustomerQueue cq = new CustomerQueue();
+        Thread pro = new producer(cq);
+        Thread con = new consumer(cq);
+        pro.setName("Producer");
+        con.setName("Consumer");
+        pro.start();
+        con.start();
 
-class producer extends Thread {
-
-	int count = 1;
-	CustomerQueue qp;
-
-	public producer(CustomerQueue q) {
-		qp = q;
-	}
-
-	@Override
-	public void run() {
-
-		while (true) {
-
-			synchronized (qp.sharedCustomerQueue) { // will throw nullPointerException if q.sharedCustomerQueue not
-													// initialized
-				if (count > ProducerConsumerQ.NEED_TO_SERVE_BATCH) {
-					System.out.println("Producer has served " + ProducerConsumerQ.NEED_TO_SERVE_BATCH + " batch");
-
-					// lets release lock before we go
-					try {
-						qp.sharedCustomerQueue.wait();
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					break;
-				}
-
-				System.out
-						.println(" Queue is empty , putting items : count " + count + " by Thread :" + this.getName());
-
-				for (int p = 0; p < 5; p++) {
-					qp.sharedCustomerQueue[p] = (int) ((Math.random() * 10) % 10) + "-cus";
-					System.out.println("Genereated item is :" + qp.sharedCustomerQueue[p]);
-				}
-
-				try {
-					count++;
-					qp.sharedCustomerQueue.wait();
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-
-		}
-
-	}
+    }
 
 }
 
-class consumer extends Thread {
+class producer extends Thread
+{
 
-	int count = 1;
-	CustomerQueue qc;
+    int count = 0;
+    CustomerQueue qp;
 
-	public consumer(CustomerQueue q) {
-		qc = q;
-	}
+    public producer(CustomerQueue q)
+    {
+        qp = q;
+    }
 
-	@Override
-	public void run() {
+    @Override
+    public void run()
+    {
 
-		while (true) {
+        while (count < ProducerConsumerQ.NEED_TO_SERVE_BATCH)
+        {
+            System.out.println("\n");
+            synchronized (qp.sharedCustomerQueue)
+            {
 
-			// Sleep is required to increase fairness of Producer Thread, Otherwise this was executing three times.
-			try {
-				sleep(2000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+                // everytime generate 5 random numbers and put in Queue
+                for (int p = 0; p < 5; p++)
+                {
+                    qp.sharedCustomerQueue[p] = (int) ((Math.random() * 10) % 10) + "";
+                    System.out.println("Producer : Genereated item is :" + qp.sharedCustomerQueue[p]);
+                }
+                qp.sharedCustomerQueue.notify();
+                try
+                {
+                    qp.sharedCustomerQueue.wait();
+                }
+                catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+                count++;
+            }
+        }
 
-			synchronized (qc.sharedCustomerQueue) {
+    }
+}
 
-				// we can check a Thread is having Lock/monitor of one object/class or not.
-				System.out.println(" consumer having lock " + holdsLock(qc.sharedCustomerQueue));
+class consumer extends Thread
+{
 
-				if (count > ProducerConsumerQ.NEED_TO_SERVE_BATCH) {
-					System.out.println("consumer has consumed " + ProducerConsumerQ.NEED_TO_SERVE_BATCH + " batch");
-					qc.sharedCustomerQueue.notify();
-					break;
-				}
+    int count = 0;
+    CustomerQueue qc;
 
-				for (String qItem : qc.sharedCustomerQueue) {
-					System.out.println("Consuming item " + qItem);
-				}
+    public consumer(CustomerQueue q)
+    {
+        qc = q;
+    }
 
-				System.out.println("\n");
-				qc.sharedCustomerQueue.notify();
-			}
+    @Override
+    public void run()
+    {
 
-			if (++count > ProducerConsumerQ.NEED_TO_SERVE_BATCH) {
-				break;
-			}
+        while (count < ProducerConsumerQ.NEED_TO_SERVE_BATCH)
+        {
+            if (qc.sharedCustomerQueue[0] != null)
+            {
+                synchronized (qc.sharedCustomerQueue)
+                {
 
-		}
+                    for (String qItem : qc.sharedCustomerQueue)
+                    {
+                        System.out.println("Consumer : Consuming item " + qItem);
+                    }
 
-	}
+                    System.out.println("\n");
+                    qc.sharedCustomerQueue.notify();
 
+                    try
+                    {
+                        qc.sharedCustomerQueue.wait();
+                    }
+                    catch (InterruptedException e)
+                    {
+                        e.printStackTrace();
+                    }
+                    count++;
+                }
+            }
+        }
+
+    }
 }
